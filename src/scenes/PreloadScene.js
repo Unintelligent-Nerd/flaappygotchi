630ca01f29b6c4724cfde2a8c0b6038deb063c89
Unintelligent-Nerd <unintelligent.nerd@gmail.com>
2021-06-10 16:23:12 +0800

    Initial Commit of Flaappygotchi Prototype

---
+++ src/scenes/PreloadScene.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser';
+
+class PreloadScene extends Phaser.Scene {
+
+    constructor(config) {
+      super('PreloadScene');
+  }
+
+    preload() {
+        this.load.image('space', 'assets/space.png');
+        // this.load.image('aavegotchi', 'assets/aavegotchi.png');
+        this.load.spritesheet('bird', 'assets/birdSprite.png', {
+            frameWidth: 16, frameHeight: 16
+        });
+        this.load.image('pipe', 'assets/pipe.png');
+        this.load.image('pause', 'assets/pause.png');
+        this.load.image('back', 'assets/back.png');
+
+        this.load.audio('music', 'assets/audio/Aavegotchi - 6. Blockchain Boléro.mp3');
+    }
+
+    create() {
+        this.scene.start('MenuScene');
+    }
+}
+
+export default PreloadScene;
